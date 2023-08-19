26a225871a115b722fe556e888babe99beea67c9
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2023-08-19 16:30:53 +0000

    4回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,36 +8,36 @@ class BooksController < ApplicationController
      render :index
     end
   end
-  
+
   def index #データの一覧を表示する
    @book = Book.new
    @index = Book.all
   end
 
   def show #データの内容（詳細）を表示する
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
 
   def edit #データを更新するためのフォームを表示する
     @book = Book.find(params[:id])
   end
-  
+
   def update #データを更新する
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id)
   end
 
   def destroy #データを削除する
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    @book.destroy  # データ（レコード）を削除
+    redirect_to books_path, notice: 'Book was successfully destroyed.' # 投稿一覧画面へリダイレクト
   end
 
   private
-   
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'#url=>ｺﾝﾄﾛｰﾗ#ｱｸｼｮﾝ
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
+  root 'homes#top'
   resources :books
-  resources :show
-  resources :index
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
